ee90905efab6bc8a34d40edcff28a9dcad57132e
kurt <kurt.lucas@gridqube.com>
2024-10-14 09:46:56 +1000

    Fixed unit tests to pass with new functionality

--- test/doeobjective.jl
+++ test/doeobjective.jl
@@ -21,9 +21,9 @@
 
         res = GPSTTopic82024.solve_mc_doe_max_pg_competitive(case1_math4w, ipopt)
 
-        @test isapprox(78.00000073488262, res["objective"]; atol=0.001)
+        @test isapprox(26.000000244958883, res["objective"]; atol=0.001)
         @test res["termination_status"] == LOCALLY_SOLVED
-        @test isapprox([gen["pg_cost"] for (g,gen) in res["solution"]["gen"]], [3.000000022473671, 0.0, 15.000000142481792, 15.00000014248179, 15.000000142481792, 15.000000142481788, 15.000000142481792]; atol=0.001)
+        @test isapprox([gen["pg_cost"] for (g,gen) in res["solution"]["gen"]], [1.0000000074908888, 0.0, 5.000000047493599, 5.000000047493599, 5.000000047493598, 5.0000000474936, 5.000000047493598]; atol=0.001)
 
         # println("Obj: $(res["objective"])")
         # println("termination: $(res["termination_status"])")
@@ -49,7 +49,7 @@
 
         res = GPSTTopic82024.solve_mc_doe_fair_pg_mse(case1_math4w, ipopt)
 
-        @test isapprox(res["objective"], 10.37500000748229; atol=0.001)
+        @test isapprox(res["objective"], -10.37500000748229; atol=0.001)
         @test res["termination_status"] == LOCALLY_SOLVED
         @test isapprox([gen["pg_cost"] for (g,gen) in res["solution"]["gen"]], [3.000000014964578, 0.0, 3.250000010465425, 3.250000010465425, 3.250000010465425, 3.250000010465421, 3.2500000104654223]; atol=0.001)
     end
@@ -73,7 +73,7 @@
 
         res = GPSTTopic82024.solve_mc_doe_fair_pg_abs(case1_math4w, ipopt)
         
-        @test isapprox(res["objective"], 39.42857183020957; atol=0.001)
+        @test isapprox(res["objective"], -39.42857183020957; atol=0.001)
         @test res["termination_status"] == LOCALLY_SOLVED
         @test isapprox([gen["pg_cost"] for (g,gen) in res["solution"]["gen"]], [3.0000000269044715, 1.1093727935885338e-37, 15.000000132458675, 15.000000132458673, 15.000000132458673, 15.000000132458673, 15.000000132458675]; atol=0.001)
     end
